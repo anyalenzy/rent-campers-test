008833d75012d0eecfb5ed785def8380681efcd7
anyalenzy <anya.dudnik.91@gmail.com>
2024-07-21 15:18:41 +0300

    done review

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,7 +1,40 @@
 import css from "./Review.module.css";
+import Icon from "../Icon/Icon";
 
 const Review = ({ review }) => {
-  return <div>Review</div>;
+  const generateIcons = (rating) => {
+    const maxRating = 5;
+    const icons = [];
+    for (let i = 1; i <= maxRating; i++) {
+      icons.push(
+        <Icon
+          key={i}
+          iconName="star"
+          width="16"
+          height="16"
+          styles={i <= rating ? css.filledIcon : css.emptyIcon}
+        />
+      );
+    }
+    return icons;
+  };
+
+  return (
+    <li>
+      <div className={css.reviewTop}>
+        <div className={css.reviewAvatar}>
+          <p className={css.name}>{review.reviewer_name[0]}</p>
+        </div>
+        <div className={css.fullRatingContainer}>
+          <p className={css.fullName}>{review.reviewer_name}</p>
+          <div className={css.rating}>
+            {generateIcons(review.reviewer_rating)}
+          </div>
+        </div>
+      </div>
+      <p className={css.comment}>{review.comment}</p>
+    </li>
+  );
 };
 
 export default Review;
